90a52d3c6d262545075fb8d26e7497ad26a5032a
Radexin <79840857+Radexin@users.noreply.github.com>
2025-05-31 21:47:05 +1000

    Bukkit player to server player

--- src/main/java/de/btegermany/terraplusminus/utils/PlayerHashMapManagement.java
+++ src/main/java/de/btegermany/terraplusminus/utils/PlayerHashMapManagement.java
@@ -1,30 +1,30 @@
 package de.btegermany.terraplusminus.utils;
 
-import org.bukkit.entity.Player;
+import net.minecraft.server.level.ServerPlayer;
 
 import java.util.HashMap;
 
 public class PlayerHashMapManagement {
 
-    HashMap<Player, String> players;
+    HashMap<ServerPlayer, String> players;
 
     public PlayerHashMapManagement() {
         players = new HashMap<>();
     }
 
-    public void addPlayer(Player player, String coordinates) {
+    public void addPlayer(ServerPlayer player, String coordinates) {
         players.put(player, coordinates);
     }
 
-    public void removePlayer(Player player) {
+    public void removePlayer(ServerPlayer player) {
         players.remove(player);
     }
 
-    public boolean containsPlayer(Player player) {
+    public boolean containsPlayer(ServerPlayer player) {
         return players.containsKey(player);
     }
 
-    public String getCoordinates(Player player) {
+    public String getCoordinates(ServerPlayer player) {
         return players.get(player);
     }
 
